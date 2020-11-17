5173a9ed09ada450fc6a1bdac9036c607c97519e
Marthe Priouret <priouretmarthe@orange.fr>
2020-11-17 11:07:21 +0000

    Quality analysis script with a for loop with automatized seach for the files

--- fastqc.sh
+++ fastqc.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# Go in the working directory
+# Go in the working directory that stores the datas
 SRA_data="/home/rstudio/data/mydatalocal/data/SRA_data"
 cd $SRA_data
 
-# Create a folder for the fastqc
+# Create a folder for the fastqc output files
 mkdir -p SRA_data_quality
 
-# Make a list of SRR accessions:
-SRR="SRR7591064.fastq
-SRR7591065.fastq
-SRR7591066.fastq
-SRR7591067.fastq
-SRR7591068.fastq
-SRR7591069.fastq"
+# Define a variable that contains the fastq files (1st way)
+#SRR="SRR7591064.fastq
+#SRR7591065.fastq
+#SRR7591066.fastq
+#SRR7591067.fastq
+#SRR7591068.fastq
+#SRR7591069.fastq"
 
-# Pour chaque fastq 
-for f in $SRR
+# Define a variable that contains the fastq files (2nd way)
+fastq=$SRA_data/*.fastq
+
+# For each fastq file (1st way): 
+#for f in $SRR
+#do
+
+# For each fastq file (2nd way):
+for f in $fastq
 do
 
 fastqc -o /home/rstudio/data/mydatalocal/data/SRA_data/SRA_data_quality -t 16 $f
