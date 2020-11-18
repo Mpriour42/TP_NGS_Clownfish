6ee69f25368ec11eaa31e0bef397fb29ffb07f26
Marthe Priouret <priouretmarthe@orange.fr>
2020-11-18 08:14:51 +0000

    Script trinity qui tourne

--- trinity.sh
+++ trinity.sh
@@ -7,10 +7,10 @@ cd $SRA_data
 # Create a folder for the trinity output files
 mkdir -p SRA_data_trinity
 
-FASTQ=$(ls $data/SRA_data/*.fastq |paste -s -d, -)
+FASTQ=$(ls $SRA_data/*.fastq |paste -s -d, -)
 
 Trinity --seqType fq --SS_lib_type R --max_memory 50G --CPU 16 --single $FASTQ \
---normalize_by_read_set --o /home/rstudio/data/mydatalocal/data/SRA_data/SRA_data_trinity/
+--normalize_by_read_set --output /home/rstudio/data/mydatalocal/data/SRA_data/SRA_data_trinity/
 
 # Remove length and path in sequence names to avoid bug with blast (sequence name length > 1000)
-sed -re "s/(>[_a-zA-Z0-9]*)( len=[0-9]*)( path=.*)/\1/"
+# sed -re "s/(>[_a-zA-Z0-9]*)( len=[0-9]*)( path=.*)/\1/"
