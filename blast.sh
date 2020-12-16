0b27d3801fc7c3cb1eb84ca5bcbe70cd94781fe6
Mpriour42 <priouretmarthe@orange.fr>
2020-12-16 15:56:27 +0000

    blast Paul

--- blast.sh
+++ blast.sh
@@ -4,7 +4,7 @@
 
 # Create a working directory:
 data="/home/rstudio/data/mydatalocal/data"
-data_reference="$data/data_reference"
+data_reference="$data/data_reference"??? Is that the stegastes_coding?
 data_blast="$data/data_blast"
 data_Trinity="$data/SRA_data/SRA_data_trinity"
 out_blast="$data/data_blast/out_blast"
@@ -23,7 +23,7 @@ makeblastdb -dbtype nucl -in $data_reference/spartitus_coding_format.fa -out $db
 #Le fichier Trinity a été renommé avec la commande :    awk '{print $1}' Trinity.fasta.transdecoder.cds > Trinity.fasta.transdecoder.rename.cds
 # e-value : si on fait seuil élévé on a plein de hits mais davantage de faux positifs. Pour homologie entre espèce on met en général 1e-10
 # outfmt donne le type de format de sortie que l'on veut, le 6 est le plus utilisé (tableau pas mal)
-blastn -db $db -query $data_transdecoder/Trinity.fasta.transdecoder.rename.cds -evalue 1e-20 -outfmt 6 -out $out_blast/blast_file
+blastn -db $db -query $data_transdecoder/Trinity.fasta.transdecoder.rename.cds -evalue 1e-10 -outfmt 6 -out $out_blast/blast_file
 # On obtient : identifiant du transcrit / gene correspondant
 # Pour déterminer le nombre de seq qui ont été alignées : cut -f1 blast_file |sort |uniq |wc -l (on compte le nombre de mots dans premiere colonne). On obitent 25000 gene, ca doit etre a peu pres nombre de genes codants dans la peau de poisson clown
 # launch script with nohup ./blast.sh >& nohup.blast &
